3197a05187fb871ac637663f62db8e29cf674399
Kortekz <87400262+Kortekz@users.noreply.github.com>
2024-03-15 18:44:00 +0200

    backend cookie

--- middleware/cartAuth.js
+++ middleware/cartAuth.js
@@ -7,7 +7,7 @@ config()
 const authenticate = (req,res,next) => {
     let {cookie} = req.headers
     let tokenInHeader = cookie && cookie.split('=')[1]
-    if(tokenInHeader===null) res.sendStatus(401)
+    if (tokenInHeader === undefined) res.sendStatus(401)
     console.log(tokenInHeader)
     jwt.verify(tokenInHeader, process.env.SECRET_KEY, (err, user)=>{
         if(err)return res.sendStatus(403)
