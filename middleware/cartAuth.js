4948e37002f30d1f3be2e0964d133ccddf00a0a0
Kortekz <cornebalie789@gmail.com>
2024-03-22 16:00:17 +0200

    New Version2.2

--- middleware/cartAuth.js
+++ middleware/cartAuth.js
@@ -5,14 +5,15 @@ config()
 const authenticate = (req,res,next) => {
     let {cookie} = req.headers
     console.log(req.headers)
-    console.log(cookie.split(';')[0].split('=')[1])
+    // console.log(cookie.split(';')[0].split('=')[1])
     let tokenInHeader = cookie && cookie.split(';')[0].split('=')[1]
     if(tokenInHeader===null) res.sendStatus(401)
     jwt.verify(tokenInHeader, process.env.SECRET_KEY, (err, user)=>{
         
         if(err)return res.sendStatus(403)
         req.user = user;
-        req.email = user.currentUser.email; // Adjusted to access email from currentUser object
+        req.email = user.currentUser.email; 
+        // this accesses email from currentUser object
         console.log(user.currentUser.email);
     next()
     })
