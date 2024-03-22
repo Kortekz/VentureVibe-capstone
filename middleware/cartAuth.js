8c105c3be8037faa3f4729ec5fb2b8a0e7efc2f9
Kortekz <cornebalie789@gmail.com>
2024-03-22 16:19:50 +0200

    New Version2.2

--- middleware/cartAuth.js
+++ middleware/cartAuth.js
@@ -8,9 +8,12 @@ const authenticate = (req,res,next) => {
     // console.log(cookie.split(';')[0].split('=')[1])
     let tokenInHeader = cookie && cookie.split(';')[0].split('=')[1]
     if(tokenInHeader===null) res.sendStatus(401)
+    console.log(tokenInHeader);
     jwt.verify(tokenInHeader, process.env.SECRET_KEY, (err, user)=>{
         console.log('Before the err 403');
         if(err)return res.sendStatus(403)
+        console.log('After the err 403');
+
         req.user = user;
         req.email = user.currentUser.email; 
         // this accesses email from currentUser object
